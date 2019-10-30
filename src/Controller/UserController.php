f3fae4088ffc7214c88c7da45a85cec9808f957f
Armoredbrain <l1ght3rth4nl1ght@gmail.com>
2019-10-30 21:20:28 +0100

    json receive controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,7 +7,7 @@ class UserController extends AbstractController
     public function signIn()
     {
         $table = file_get_contents('php://input');
-        $jsonObject = json_decode($table);
+        $jsonObject = json_decode($table, true);
         var_dump($jsonObject);
         //die();
     }
