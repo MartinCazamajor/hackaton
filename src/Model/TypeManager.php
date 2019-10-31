aaf9114593e21e4329d9307d5fbfdeb35694cbf1
Martin Cazamajor <martin.cazamajor@gmail.com>
2019-10-31 04:13:59 +0100

    route user/logIn fonctionnelle en back, reçoit le front et renvoie une réponse

---
+++ src/Model/TypeManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+class TypeManager extends AbstractManager
+{
+    const TABLE = 'type';
+
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+}
